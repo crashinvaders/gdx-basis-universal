e8be4659e94572b0b0ed286cf3dfceae607e7c7f
Anton Chekulaev <metaphore@Metaphores-iMac-Pro.local>
2023-09-02 18:41:46 +0200

    Adapt to new jnigen.

--- assemble-natives-macos.sh
+++ assemble-natives-macos.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Should executed on MacOS only.
-./gradlew jnigen jnigenBuildMacOsX64 jnigenJarNativesDesktop
+./gradlew jnigen jnigenBuildMacOsX64 jnigenBuildMacOsXARM64 jnigenJarNativesDesktop
